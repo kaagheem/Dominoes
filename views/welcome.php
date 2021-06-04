50085b155053c9a04be7e38bbb946f938d842e3d
Kareem <67461927+kaagheem@users.noreply.github.com>
2021-06-04 02:01:14 +0200

    Create welcome.php

---
+++ views/welcome.php
@@ -0,0 +1,8 @@
+<div class="home">
+  <img src="./assets/images/welcome/bg.jpg" alt="">
+  <div class="content">
+    <h2>Let's Make<br>Something Creative</h2>
+    <p>PHP Model Views Controller Mini Framework , <br>For Educational use.</p>
+    <a href="/start">Get Started</a>
+  </div>
+</div>
